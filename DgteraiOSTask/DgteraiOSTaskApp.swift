f462c889038d2527ff43d05637320c1dc1d7ae8b
Ahmed Mahrous <ahmed.mahrous@gomini-app.com>
2024-04-17 07:50:47 +0200

    Initial Commit

---
+++ DgteraiOSTask/DgteraiOSTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DgteraiOSTaskApp.swift
+//  DgteraiOSTask
+//
+//  Created by Ahmed Mahrous on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DgteraiOSTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
